fbedc0eef692e5556152b186186b757855b4c41f
Yilin YE <42186860+yiliny@users.noreply.github.com>
2022-08-21 22:46:06 +0200

    Add files via upload

--- FORTRAN/para.h
+++ FORTRAN/para.h
@@ -1,5 +1,36 @@
-real*8 rayon,rhosty,rhosol,mass,mz,mx,mt,grav,kappa,eps,xi,kxi,kxe,temp,k_B,beta,clight,dt
-common/para/rayon,rhosty,rhosol,mass,mz,mx,mt,grav,kappa,eps,xi,kxi,kxe,temp,k_B,beta,clight,dt
+!!!!! We define parameters used for equations
+real*8 rayon,rhosty,rhosol,mass,mz,mx,mt,grav,kappa,eps,xi,kxi,kxe,temp,k_B,beta,clight,dt,l_D,csquare
+common/para/rayon,rhosty,rhosol,mass,mz,mx,mt,grav,kappa,eps,xi,kxi,kxe,temp,k_B,beta,clight,dt,l_D,csquare
 
-real*8 Minv,gamma,intma,intmb
-common/calcul/Minv,gamma,intma,intmb
+!! rayon: radius of particle
+!! rhosty: density of styrene
+!! rhosol: density of solvent (water)
+!! mass: mass of particle
+!! mz,mx,mt: mass along different direction
+!! grav: gravity acceleration
+!! kappa: compliance
+!! eps: dimensionless parameter
+!! xi: dimensionless viscosity
+!! kxi = kappa*xi
+!! kxe = kappa*xi*eps
+!! temp: temperature
+!! k_B: Boltzmann constant
+!! beta = 1 / (temp*k_B)
+!! clight: max falling speed of particle
+!! dt: time gap for numerical simulations
+!! l_D: Debye length for electrostatic repulsion
+!! csquare = clight**2
+
+
+
+
+
+
+!!!!! Then there leaves some ancient parameters useless.
+!real*8 Minv,gamma,intma,intmb
+!common/calcul/Minv,gamma,intma,intmb
+real*8 noise(3,2),Minv(3,3),gmaeff(3,3,3)
+common/calcul/noise,Minv,gmaeff
+!! "noise" noise = random force; 3: z/x/Θ; 2: Box-Muller method
+!! "Minv" effective mass matrix inverse; 3,3: matrix index
+!! "gmaeff" effective friction matrix; 3: gamma0/gamma1/gamma1v; 3,3: matrix index
